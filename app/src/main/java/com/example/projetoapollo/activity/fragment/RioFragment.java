7a48084958baa1175d940c2b3fcc601f0ee2d3e3
migzviana <backupdosjogos2@gmail.com>
2024-05-31 18:48:37 -0300

    update modais

--- app/src/main/java/com/example/projetoapollo/activity/fragment/RioFragment.java
+++ app/src/main/java/com/example/projetoapollo/activity/fragment/RioFragment.java
@@ -4,40 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.projetoapollo.R;
-import com.example.projetoapollo.activity.adapters.ImagesAdapter;
-import com.example.projetoapollo.activity.fragment.ImageDialogFragment;
 
 public class RioFragment extends Fragment {
 
-    private final int[] imageResources = {
-            R.drawable.hotel_brasilia1,
-            R.drawable.hotel_brasilia2,
-            R.drawable.hotel_brasilia3,
-            R.drawable.tourist_brasilia1,
-            R.drawable.tourist_brasilia2,
-            R.drawable.tourist_brasilia3,
-            R.drawable.tourist_brasilia4
-    };
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_brasilia, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewImages);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2)); // Define 2 colunas
-        ImagesAdapter adapter = new ImagesAdapter(getContext(), imageResources);
-        recyclerView.setAdapter(adapter);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_sao_paulo, container, false);
     }
 }
--- app/src/main/java/com/example/projetoapollo/activity/fragment/SaoPauloFragment.java
+++ app/src/main/java/com/example/projetoapollo/activity/fragment/SaoPauloFragment.java
@@ -1,43 +1,116 @@
 package com.example.projetoapollo.activity.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
 
 import com.example.projetoapollo.R;
-import com.example.projetoapollo.activity.adapters.ImagesAdapter;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class SaoPauloFragment extends Fragment {
 
-    private final int[] imageResources = {
-            R.drawable.hotel_brasilia1,
-            R.drawable.hotel_brasilia2,
-            R.drawable.hotel_brasilia3,
-            R.drawable.tourist_brasilia1,
-            R.drawable.tourist_brasilia2,
-            R.drawable.tourist_brasilia3,
-            R.drawable.tourist_brasilia4
-    };
+    private String city = "SaoPaulo"; // Defina a cidade baseada na sua lógica
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_brasilia, container, false);
+        View view = inflater.inflate(R.layout.fragment_sao_paulo, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewImages);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2)); // Define 2 colunas
-        ImagesAdapter adapter = new ImagesAdapter(getContext(), imageResources);
-        recyclerView.setAdapter(adapter);
+        // Configurar listeners para os ImageViews
+        view.findViewById(R.id.imageView1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
+
+        view.findViewById(R.id.imageView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
+
+        view.findViewById(R.id.imageView3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
+
+        view.findViewById(R.id.imageView4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
+
+        view.findViewById(R.id.imageView5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
+
+        view.findViewById(R.id.imageView6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagesModal();
+            }
+        });
 
         return view;
     }
+
+    private void showImagesModal() {
+        Dialog dialog = new Dialog(getActivity());
+
+        int layoutId;
+        switch (city) {
+            case "SaoPaulo":
+                layoutId = R.layout.dialog_sao_paulo;
+                break;
+            case "Brasilia":
+                layoutId = R.layout.dialog_brasilia;
+                break;
+            case "RioDeJaneiro":
+                layoutId = R.layout.dialog_rio;
+                break;
+            default:
+                layoutId = R.layout.dialog_sao_paulo;
+                break;
+        }
+        dialog.setContentView(layoutId);
+
+        switch (city) {
+            case "SaoPaulo":
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage1)).setImageResource(R.drawable.hotel_sp4);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage2)).setImageResource(R.drawable.hotel_sp1);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage3)).setImageResource(R.drawable.hotel_sp2);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage4)).setImageResource(R.drawable.tourist_sp1);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage5)).setImageResource(R.drawable.tourist_sp2);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage6)).setImageResource(R.drawable.tourist_sp3);
+                break;
+            case "Brasilia":
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage1)).setImageResource(R.drawable.hotel_brasilia1);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage2)).setImageResource(R.drawable.hotel_brasilia2);
+                // Definir outras imagens para Brasília
+                break;
+            case "RioDeJaneiro":
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage1)).setImageResource(R.drawable.hotel_rio1);
+                ((ShapeableImageView) dialog.findViewById(R.id.modalImage2)).setImageResource(R.drawable.hotel_rio2);
+                // Definir outras imagens para Rio de Janeiro
+                break;
+        }
+
+        dialog.show();
+    }
 }
+
